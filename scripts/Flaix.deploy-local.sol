2c121c5594580020fc4f64fd5dab4c0e2044fa19
Ned Albo <ned.albo@mailfence.com>
2023-03-04 15:02:49 +0100

    updated local deploy script

--- scripts/Flaix.deploy-local.sol
+++ scripts/Flaix.deploy-local.sol
@@ -3,11 +3,19 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
 import "@src/flaix/FlaixVault.sol";
+import "@src/flaix/FlaixCallOption.sol";
+import "@src/flaix/FlaixPutOption.sol";
+import "@src/flaix/FlaixOptionFactory.sol";
 
 contract FlaixDeployLocal is Script {
   function run() public {
     vm.startBroadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
-    FlaixVault vault = new FlaixVault();
+    FlaixCallOption callOption = new FlaixCallOption();
+    FlaixPutOption putOption = new FlaixPutOption();
+    FlaixOptionFactory optionFactory = new FlaixOptionFactory(address(callOption), address(putOption));
+
+    FlaixVault vault = new FlaixVault(address(optionFactory));
+    console.log("FlaixVault deployed at", address(vault));
     vm.stopBroadcast();
   }
 }
